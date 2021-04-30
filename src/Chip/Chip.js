9cb432a62d92f7a6252743316ed0f7c17e160ccc
CrazyRedTime <zubbol89@gmail.com>
2021-05-01 00:22:56 +0400

    almost done

--- src/Chip/Chip.js
+++ src/Chip/Chip.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import { useEffect } from "react/cjs/react.development";
 
+import styles from './Chip.module.css'
+
 const Chip = ({ value, index, chipsArray, setChipsArray }) => {
 
   const [chipValue, setChipValue] = useState(value);
+
+  useEffect(() => {
+    setChipValue(value);
+  }, [value]);
 
   const saveChipValue = (e) => {
     setChipValue(e.target.innerText);
@@ -17,15 +23,26 @@ const Chip = ({ value, index, chipsArray, setChipsArray }) => {
     }
   }, [chipValue, value, chipsArray, index, setChipsArray])
 
+  const deleteChip = (e) => {
+    e.preventDefault();
+    const copyOfChipsArray = [...chipsArray];
+    copyOfChipsArray.splice(index, 1);
+    setChipsArray(copyOfChipsArray);
+  }
+
   return (
-    <div>
+    <div className={styles.chipContainer}>
       <span
+        className={styles.chip}
         contentEditable
         suppressContentEditableWarning
         onBlur={saveChipValue}
       >
         {value}
       </span>
+      <button className={styles.deleteButton} onClick={deleteChip}>
+        x
+      </button>
     </div>
   );
 };
--- src/ChipsInput/ChipsInput.js
+++ src/ChipsInput/ChipsInput.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import Chip from "../Chip/Chip";
 
+import styles from "./ChipsInput.module.css";
+
 const ChipsInput = ({ value, setValue }) => {
   const makeArrayFromString = (string) => {
     return string
@@ -11,13 +13,11 @@ const ChipsInput = ({ value, setValue }) => {
 
   const [chipsArray, setChipsArray] = useState(makeArrayFromString(value));
 
-  useEffect(() => {
-    console.log(makeArrayFromString(value));
-  }, [value]);
-
   const [inputValue, setInputValue] = useState("");
 
   const [hasOpenedQuotes, setHasOpenedQuotes] = useState(false);
+
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
     const re = /"/g;
@@ -32,6 +32,7 @@ const ChipsInput = ({ value, setValue }) => {
     const newString = chipsArray.join(", ");
     if (value !== newString) {
       setValue(newString);
+      setChipsArray(makeArrayFromString(newString));
     }
   }, [value, chipsArray, setValue]);
 
@@ -41,33 +42,58 @@ const ChipsInput = ({ value, setValue }) => {
       setChipsArray([...chipsArray, inputValue]);
       setInputValue("");
     }
+    if (e.keyCode === 8 && !inputValue) {
+      const copyOfChipsArray = [...chipsArray];
+      copyOfChipsArray.splice(copyOfChipsArray.length - 1, 1);
+      setChipsArray(copyOfChipsArray);
+    }
   };
 
-  const hadleInput = (e) => {
+  const handleInput = (e) => {
     if (hasOpenedQuotes) {
       setInputValue(e.target.value);
     } else if (!hasOpenedQuotes && e.nativeEvent.data !== ",") {
       setInputValue(e.target.value);
     }
+    setHasError(false);
   };
 
+  const blurInput = () => {
+    if (hasOpenedQuotes) {
+      setHasError(true);
+    } else if (inputValue) {
+      setChipsArray([...chipsArray, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const placeholder = value ? null : "введите ключевые слова";
+
   return (
-    <div>
-      {chipsArray.map((chip, index) => (
-        <Chip
-          value={chip}
-          key={index}
-          index={index}
-          chipsArray={chipsArray}
-          setChipsArray={setChipsArray}
+    <div className={styles.inputContainer}>
+      <div className={styles.inputDiv}>
+        {chipsArray.map((chip, index) => (
+          <Chip
+            value={chip}
+            key={index}
+            index={index}
+            chipsArray={chipsArray}
+            setChipsArray={setChipsArray}
+          />
+        ))}
+        <input
+          className={styles.rootInput}
+          placeholder={placeholder}
+          type="text"
+          onChange={handleInput}
+          onKeyDown={addChip}
+          value={inputValue}
+          onBlur={blurInput}
         />
-      ))}
-      <input
-        type="text"
-        onChange={hadleInput}
-        onKeyDown={addChip}
-        value={inputValue}
-      />
+      </div>
+      {hasError ? (
+        <span className={styles.error}>Закройте кавычки с двух сторон</span>
+      ) : null}
     </div>
   );
 };
